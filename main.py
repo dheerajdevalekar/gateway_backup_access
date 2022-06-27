b2b78931a8c73b76a2db09e83b4c7bc30a1ecfb0
Dheeraj-New-PC <DHEERAJ1423@gitlab.com>
2022-06-27 12:32:40 +0530

    Write the Files using os.system() Logic

---
+++ main.py
@@ -0,0 +1,23 @@
+from loguru import logger
+from os import getcwd, system, path
+from datetime import datetime
+
+
+def main():
+    try:
+        path_ = f"{getcwd()}/logs"
+        curr_time = datetime.now().strftime("%Y-%m-%d_%H:%M:%S")  # This strftime is used to Filter out the Microseconds from Main Time
+        cmd = f"dmesg -w > {path_}/{curr_time}.txt"
+        if path.exists(path_) is True:
+            logger.info(f"{path_} Folder Already Exist")
+            system(cmd)
+        else:
+            system(f"mkdir {path_}")
+            logger.info(f"{path_} Folder Created")
+            system(cmd)
+    except Exception as e:
+        logger.error(f"{e}")
+
+
+if __name__ == '__main__':
+    main()
